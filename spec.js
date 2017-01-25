e428f262d351a5ef0373e39552f71afb01281ca1
Bhargavi-Kodidela <https://bhargavik123@bitbucket.org/wikima4Devteam/mesaforte-rest.git/>
2017-01-25 15:17:37 +0530

    modified files

--- spec.js
+++ spec.js
@@ -14,12 +14,20 @@ describe('Mesaforte-ui Demo App', function(){
 
 
 		//browser.wait(element(by.css(".topnavbar")).isPresent());
-		expect(element(by.model('beanCtrl.gridOptions.quickFilterText')).isPresent()).toBe(true);
+		//Filter Results
+		/*expect(element(by.model('beanCtrl.gridOptions.quickFilterText')).isPresent()).toBe(true);
 		element(by.model('beanCtrl.gridOptions.quickFilterText')).sendKeys('dent');
+		//Search
+	    expect(element(by.model('beanCtrl.pages.category')).isPresent()).toBe(true);
+	    element(by.model('beanCtrl.pages.category')).sendKeys('built-in');
+	    element(by.buttonText('Search')).click();*/
 
+	    //Add button
+	    expect(element(by.buttonText('Add')).isPresent()).toBe(true);
+	    element(by.buttonText('Add')).click();
 
-
-
+	    expect(element(by.model('rulesset.newBean.names_lang[vm.currentLang].text1')).isPresent()).toBe(true);
+	    element(by.model('rulesset.newBean.names_lang[vm.currentLang].text')).sendKeys('built');
 
 		/*browser.driver.wait(function() {
             return element(by.css('topnavbar')).isDisplayed().then(function(IsVisible) {
